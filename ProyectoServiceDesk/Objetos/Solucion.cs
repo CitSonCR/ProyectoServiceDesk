88527d6651359e24c305a6895f60028ac2c25a02
RobertoAlvarezMedina <robert10alvarz@gmail.com>
2018-10-22 12:38:29 -0600

    Solucion

--- ProyectoServiceDesk/Objetos/Solucion.cs
+++ ProyectoServiceDesk/Objetos/Solucion.cs
@@ -8,5 +8,21 @@ namespace ProyectoServiceDesk_Controller
 {
     class Solucion
     {
+        public String Respuesta { get; set; }
+        public String Tipo { get; set; }
+        public String Atendidopor { get; set; }
+        public int NumeroConfirmacion { get; set; }
+        public DateTime DiaHora { get; set; }
+        public String Receptor { get; set; }
+
+        public Solucion(string respuesta, string tipo, string atendidopor, int numeroConfirmacion, DateTime diaHora, string receptor)
+        {
+            Respuesta = respuesta;
+            Tipo = tipo;
+            Atendidopor = atendidopor;
+            NumeroConfirmacion = numeroConfirmacion;
+            DiaHora = diaHora;
+            Receptor = receptor;
+        }
     }
 }
