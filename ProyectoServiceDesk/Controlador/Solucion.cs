86a46b7bd39ec55d467a11e150d31e92a85985c2
RobertoAlvarezMedina <robert10alvarz@gmail.com>
2018-10-24 22:20:32 -0600

    Cambios listos_

---
+++ ProyectoServiceDesk/Controlador/Solucion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoServiceDesk.Controlador
+{
+    class Solucion
+    {
+        public int MyProperty { get; set; }
+    }
+}
