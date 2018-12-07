caeba06e8816bee90bb59f14309c0f95bba4923c
RobertoAlvarezMedina <robert10alvarz@gmail.com>
2018-12-06 22:16:12 -0600

    c

--- ProyectoServiceDesk_Logic/LogicaUsuario.cs
+++ ProyectoServiceDesk_Logic/LogicaUsuario.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ProyectoServiceDesk.Controlador;
+using ProyectoServiceDesk_Controller;
 using ProyectoServiceDesk.Modelo;
 using ProyectoServiceDesk.Utils;
 
